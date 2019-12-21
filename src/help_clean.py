07c24a1eed12875b5650d411d6ce2cada02b0951
Youngjung Yoon <youngjungyoon@Youngjungs-MacBook-Air.local>
2019-12-21 14:23:01 -0800

    add fix and cleanup functions to aid the creation of the ff matchup df

--- src/help_clean.py
+++ src/help_clean.py
@@ -23,9 +23,24 @@ def create_allmatchups_df(data):
 			count+=1
 		else:
 			all_matchups = pd.concat((all_matchups, v), axis=0, sort=False)
-	return all_matchups.reset_index().drop('index', axis=1)
+	return all_matchups.reset_index().drop(['index','QB1'], axis=1)
 
 
+def fix_team_names(data):
+	matt_name = lambda x: 'Post Mahomes' if x == 'Seattle Post Mahomes' else x
+	danny_name = lambda x: '2 Girls 1 Kupp' if x == 'urf Munchers' else x
+	data['team'] = data['team'].map(matt_name)
+	data['team'] = data['team'].map(danny_name)
+	return data
+
+def add_gm_names(data):
+	gms = ['junghwan', 'charlie', 'matt', 'mike', 'anil', 'kyle', 'sunjin',
+			 'eugene','connor', 'zach', 'kai', 'danny']
+	gms_teams = {gm:team for gm, team in zip(gms,all_match['team'].unique())}
+	teams_gms = {v:k for k,v in gms_teams.items()}
+	data['team_gm'] = data['team'].map(lambda x: teams_gms[x])
+
+	return data
 
 
 
